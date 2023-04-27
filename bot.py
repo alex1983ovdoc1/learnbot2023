f40bd79348534cf186f97e2d7a58f622a9feaa3f
alex1983ovdoc1 <bondar1983ovdoc1@gmail.com>
2023-04-27 13:53:43 +0300

    Cats, keyboards, gitignore

--- bot.py
+++ bot.py
@@ -1,5 +1,11 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from glob import glob
 import logging
+from random import choice
+
+from emoji import emojize
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler
+
 import settings0
 
 
@@ -10,28 +16,98 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 					filename='bot.log')
 
 
-def greet_user(bot, update):
-	text = 'clicked/start'
+
+# command /start don't work
+def greet_user1(bot, update, user_data):
+	print('Working com /start (gree_user1')
+
+
+# command /start1	
+def greet_user(bot, update, user_data):	
+	emo = get_user_emo(user_data)
+	# emo = emojize(choice(settings0.USER_EMOJI))
+	user_data['emo'] = emo
+	text = ' Hello {} (user clicked/start0)'.format(emo)
 	print(text)
-	update.message.reply_text(text)
+	
+	update.message.reply_text(text, reply_markup=get_keyboard())
 	text0 = 'User /{}/: "{}"'.format(update.message.chat.first_name, text)
 	logging.info(text0)
 
 
-def talk_to_me(bot, update):
+
+# user send text
+def talk_to_me(bot, update, user_data):
 	# user_text = update.message.text
-	user_text = 'User /{}/ sended: "{}"'.\
-	format(update.message.chat.first_name, update.message.text)
+	user_text = 'User {}*{} sended: "{}"'.\
+	format(update.message.chat.first_name, user_data['emo'], update.message.text)
 	# print(update.message)
 	# print()
 	print(user_text)
-	update.message.reply_text(user_text)
+	update.message.reply_text(user_text, reply_markup=get_keyboard())
 	# logging.info(user_text)
 	logging.info("User: %s, Chat id: %s, Message: %s",
 		update.message.chat.first_name, update.message.chat.id,
 		update.message.text)
 
 
+# send photo to user
+def send_cat_picture(bot, update, user_data):
+	cat_list = glob('images/cat*.jp*g')
+	cat_pic = choice(cat_list)
+	print(cat_pic)
+	bot.send_photo(chat_id=update.message.chat.id,
+	 photo=open(cat_pic, 'rb'), reply_markup=get_keyboard())
+
+
+# change avatar
+def change_avatar(bot, update, user_data):
+	if 'emo' in user_data:
+		del user_data['emo']
+	emo = get_user_emo(user_data)
+	update.message.reply_text('Ready: {}'.format(emo), reply_markup=get_keyboard())
+
+
+# users contacts
+def get_contact(bot, update, user_data):
+	print(update.message.contact)
+	update.message.reply_text('Ready! {}'.format(get_user_emo(user_data)), reply_markup=get_keyboard())
+
+
+# users location
+def get_location(bot, update, user_data):
+	print(update.message.location)
+	update.message.reply_text('Ready! {}'.format(get_user_emo(user_data)), reply_markup=get_keyboard())
+
+
+def get_keyboard():
+	contact_button = KeyboardButton('Send contacts', request_contact=True)
+	location_button = KeyboardButton('Send coordinates', request_location=True)
+	my_keyboard = ReplyKeyboardMarkup([
+		['', '-start bot-', ''],
+		['Send a cat', 'Change avatar'],
+		[contact_button, location_button]
+		], resize_keyboard=True
+		)
+	return my_keyboard
+
+
+# take emo
+def get_user_emo(user_data):
+	if 'emo' in user_data:
+		return user_data['emo']
+	else:
+		user_data['emo'] = emojize(choice(settings0.USER_EMOJI))
+		return user_data['emo']
+
+
+# Strart bot
+def start_bot(bot, update, user_data):
+	update.message.reply_text('/start0', reply_markup=get_keyboard())
+
+
+
+# bot function
 def main():
 	mybot = Updater(settings0.API_KEY)
 	# mybot = Updater((API_KEY, request_kwargs = PROXY)
@@ -41,8 +117,16 @@ def main():
 	logging.info(text2)	
 
 	dp = mybot.dispatcher
-	dp.add_handler(CommandHandler('start', greet_user))
-	dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+	dp.add_handler(CommandHandler('start', greet_user1, pass_user_data=True))
+	dp.add_handler(CommandHandler('start0', greet_user, pass_user_data=True))
+	dp.add_handler(CommandHandler('cat', send_cat_picture, pass_user_data=True))
+	dp.add_handler(RegexHandler('^(-start bot-)$', start_bot, pass_user_data=True))
+	dp.add_handler(RegexHandler('^(Send a cat)$', send_cat_picture, pass_user_data=True))
+	dp.add_handler(RegexHandler('^(Change avatar)$', change_avatar, pass_user_data=True))
+	dp.add_handler(MessageHandler(Filters.contact, get_contact, pass_user_data=True))
+	dp.add_handler(MessageHandler(Filters.location, get_location, pass_user_data=True))
+
+	dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
 
 
 	mybot.start_polling()
@@ -68,5 +152,11 @@ print('''
 	13) .info(text)      - save to file.log
 	14) MessageHandler() - handlers messages
 	15) Filters()        - filter massages-text, foto, ...
+	16) glob('/cat*.jp*g')- take list(files)
+	17) choice(list)     - random choice
+	18) .send_photo(id=,)- send photo
+	19) imojize()        - emoticons
+	20) ReplyKeyboardMarkup(^ $)- keyboard ^ start str, $ finish
+	21) KeyboardButton   - use the keyboards button ('Send contacts', 'Send coordinats')
 
 	''')
